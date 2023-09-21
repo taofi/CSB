56b53c0fbd69c8d970bb7d02f8c3e83f5499edab
taofi <taofi.stop.lego@gmail.com>
2023-09-21 21:45:38 +0300

    firstStep

--- SE_Lab09/SE_Lab09/varparm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdarg>
-#include <iostream>
-
-int ivarparm(int n, ...);
-short svarparm(short n ...);
-float fvarparm(float count, ...);
-double dvarparm(double count, ...);
